fada200746fec38d642576d7805a7de2d5105e4d
Hunter Logan <Hunterlogan@outlook.com>
2019-10-28 17:02:07 -0400

    Updating some tweets fields

--- db_wrapper/sql/V1__Init__.sql
+++ db_wrapper/sql/V1__Init__.sql
@@ -1,7 +1,4 @@
 /* https://www.db-fiddle.com/f/rNfGmis7UWefGQrFFciMqq/2 */
-
-CREATE TYPE sentiment AS ENUM ('Neutral', 'Positive', 'Negative') ;
-
 CREATE TABLE keywords ( -- Represent the words that are being tracked e.g. #China
 	id SERIAL PRIMARY KEY,
   	word TEXT UNIQUE NOT NULL
@@ -11,6 +8,9 @@ CREATE TABLE tweets (
 	tweet_id BIGINT PRIMARY KEY, -- ID from the tweet
   	keyword INTEGER REFERENCES keywords(id), -- The keyword that this tweet belongs to
   	tweet_text TEXT NOT NULL,
-  	sentiment sentiment, -- Sentiment for after processing
+	positive DECIMAL,
+	negative DECIMAL,
+	neutral DECIMAL,
+	time_tweeted TIMESTAMP,
   	resp JSONB -- The full resp of the tweet from the api call (incase there's fields we want later)
 );
--- db_wrapper/test/test_data.sql
+++ db_wrapper/test/test_data.sql
@@ -3,5 +3,5 @@
 INSERT INTO keywords (word)
 VALUES('Pizza');
 
-INSERT INTO tweets (tweet_id, keyword, tweet_text, sentiment)
-VALUES (1125490788736032770, 1, 'Today''s new update means that you can finally add Pizza Cat to your Retweet with comments! Learn more about this ne… https://t.co/Rbc9TF2s5X', 'Positive');
+INSERT INTO tweets (tweet_id, keyword, tweet_text, positive, negative, neutral)
+VALUES (1125490788736032770, 1, 'Today''s new update means that you can finally add Pizza Cat to your Retweet with comments! Learn more about this ne… https://t.co/Rbc9TF2s5X', .5, .5, .5);
