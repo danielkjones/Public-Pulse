e0bed469174b73b6d7d2f9ebbc14d8971ad72003
Matt Thyng <mattthyng@gmail.com>
2019-11-07 21:51:27 -0500

    fixed SQL insert in lambda

--- sentiment_analyzer/lambda/comprehend_query.py
+++ sentiment_analyzer/lambda/comprehend_query.py
@@ -8,16 +8,17 @@ creds = "dbname='tweet_db' user='adminuser' host='tweetdb.c0dnyygelykr.us-east-1
 
 
 def lambda_handler(event, context):
-    sentiment = json.dumps(comprehend.detect_sentiment(Text=event['tweet_text'], LanguageCode='en'), sort_keys=True, indent=4)
-    print(sentiment)
+    sentiment = comprehend.detect_sentiment(Text=event['tweet_text'], LanguageCode='en')
     try:
         conn = psycopg2.connect(creds)
         cursor = conn.cursor()
-        insert = ("INSERT INTO tweets VALUES(%d,%d,%s,%d,%d,%d,%s,%s)"
-                    % event['tweet_id'], event['keyword_id'], event['tweet_text'],
-                    sentiment['SentimentScore']['Positive'], sentiment['SentimentScore']['Negative'],
-                    sentiment['SentimentScore']['Neutral'], event['time_tweeted'], JSON.stringify(event));
-        cursor.execute(insert)
+        cursor.execute("""
+            INSERT INTO tweets
+            VALUES(%d,%d,%s,%d,%d,%d,%s,%s)
+            """,
+            event['tweet_id'], event['keyword_id'], event['tweet_text'],
+            sentiment['SentimentScore']['Positive'], sentiment['SentimentScore']['Negative'],
+            sentiment['SentimentScore']['Neutral'], event['time_tweeted'], event)
     except Exception as e:
         print(e)
     return sentiment
