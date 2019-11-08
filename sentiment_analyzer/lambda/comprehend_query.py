36f57cc02667457eb824e5047346dea79a502fee
Matt Thyng <mattthyng@gmail.com>
2019-11-08 10:43:11 -0500

    psycopg2 now works, must run in python 3.6 environment

--- sentiment_analyzer/lambda/comprehend_query.py
+++ sentiment_analyzer/lambda/comprehend_query.py
@@ -4,21 +4,23 @@ import psycopg2
 
 
 comprehend = boto3.client('comprehend')
-creds = "dbname='tweet_db' user='adminuser' host='tweetdb.c0dnyygelykr.us-east-1.rds.amazonaws.com' password=''"
+creds = "dbname=tweet_db user=adminuser host=tweetdb.c0dnyygelykr.us-east-1.rds.amazonaws.com password=adminpass"
 
 
 def lambda_handler(event, context):
     sentiment = comprehend.detect_sentiment(Text=event['tweet_text'], LanguageCode='en')
     try:
+        print('here')
         conn = psycopg2.connect(creds)
         cursor = conn.cursor()
         cursor.execute("""
             INSERT INTO tweets
-            VALUES(%d,%d,%s,%d,%d,%d,%s,%s)
+            VALUES(%s,%s,%s,%s,%s,%s,%s,%s)
             """,
-            event['tweet_id'], event['keyword_id'], event['tweet_text'],
+            (event['tweet_id'], event['keyword_id'], event['tweet_text'],
             sentiment['SentimentScore']['Positive'], sentiment['SentimentScore']['Negative'],
-            sentiment['SentimentScore']['Neutral'], event['time_tweeted'], event)
+            sentiment['SentimentScore']['Neutral'], event['time_tweeted'], json.dumps(event)))
+        conn.commit()
     except Exception as e:
         print(e)
     return sentiment
