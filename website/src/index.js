fd6d52e6e327a54c309021c11bb3d6dd7aa38dad
Brady Brown <bbrown30@massmutual.com>
2019-11-20 17:14:20 -0500

    change hostname to host

--- website/src/index.js
+++ website/src/index.js
@@ -11,7 +11,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-  uri: `http://${window.location.hostname}:3000/graphql`
+  uri: `http://${window.location.host}/graphql`
 });
 
 ReactDOM.render(
